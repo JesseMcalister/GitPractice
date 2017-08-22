94749d163ccc0e8b5a67c3a4a7ddf057b4632f73
Jesse McAlister <jessemcalister@Jesses-MacBook-Pro.local>
2017-08-22 12:28:12 -0600

    Integrated the use of the Whatsup page by adding a route and wassup view

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/hello/hi', to: 'hello#hi'
+  get '/whatsup/wassup', to: 'whatsup#wassup'
 end
